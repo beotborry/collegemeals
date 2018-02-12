05f7206ade4f77c37cdf0bd27b0eb0271817d93a
parkjh9229 <33710525+beotborry@users.noreply.github.com>
2018-02-12 10:36:04 +0900

    Update settings.py

--- snu/settings.py
+++ snu/settings.py
@@ -4,7 +4,7 @@ COLLEGE_NAME = "snu"
 
 GRADUATE_DORM_RESTAURANTS = ['대학원기숙사식당']
 
-SNUCO_RESTAURANTS = []
+SNUCO_RESTAURANTS = ['학생회관 식당', '농생대 3식당','919동 기숙사 식당', '자하연 식당', '302동 식당', '동원관 식당', '감골 식당', '사범대 4식당', '두레미담', '301동 식당', '예술계복합연구동 식당', '샤반', '공대간이식당', '소담마루', '220동 식당']
 
 VET_RESTAURANTS = ['수의대식당']
 
